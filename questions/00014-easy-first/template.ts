b3ed51c2f370d4c562989ae50bfec96837d10eae
holazz <2418184580@qq.com>
2022-05-22 14:06:43 +0800

    chore: unknown instead of any

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,3 +1,3 @@
-// type First<T extends any[]> = T[number] extends never ? never: T[0]
+// type First<T extends unknown[]> = T[number] extends never ? never: T[0]
 
-type First<T extends any[]> = T extends [] ? never: T[0]
+type First<T extends unknown[]> = T extends [] ? never: T[0]
