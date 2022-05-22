51afadf2bd240f3150e57e1e17cae06dfd641ac9
holazz <2418184580@qq.com>
2022-05-22 15:08:59 +0800

    chore: includes

---
+++ questions/00898-easy-includes/template.ts
@@ -0,0 +1,11 @@
+// type Includes<T extends readonly unknown[], U> = U extends T[number] ? true : false
+
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+
+type Includes<T extends readonly unknown[], U> = T extends [infer F, ...infer R]
+  ? Equal<F, U> extends true
+    ? true
+    : Includes<R, U>
+  : false
